5c03156752c8725db701ce035d64b0c3f924f4fe
JavaCommons Technologies <javacommons@gmail.com>
2024-05-19 00:38:58 +0900

    .

--- 32bit.py
+++ 32bit.py
@@ -1,7 +1,16 @@
 from cffi import FFI
+import json
+
 ffi = FFI()
 ffi.cdef("const char *GetMacAddressString();")
 clib = ffi.dlopen("csharp/x86/MacAddress.dll")
 
 s = ffi.string(clib.GetMacAddressString()).decode()
 print("s={}".format(s))
+
+pyobj = json.loads(s)
+print(f"pyobj={pyobj}")
+
+for nic in pyobj:
+    print(f'nic.name={nic['name']}')
+    print(f'nic.addr={nic['addr']}')
